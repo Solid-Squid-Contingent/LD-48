2f141850d3ad51e72b355c07ac3d7c692242fce2
Vincent <vincopi@googlemail.com>
2021-04-26 22:56:14 +0200

    improve ? lighting

--- scripts/Lamp.gd
+++ scripts/Lamp.gd
@@ -1,5 +1,13 @@
 extends Node2D
 
+var noise = OpenSimplexNoise.new()
+
+
+var value = 0
+
+func _ready():
+	noise.period = 16.0 
+
 func getIcon():
 	return $Sprite.texture 
 	
@@ -8,3 +16,11 @@ static func wallSnap():
 	
 static func getPrice():
 	return 1
+
+func _physics_process(delta):
+	value += 1
+	if value > 10000:
+		value = 0
+	var alpha = noise.get_noise_1d((value + 1) / 10.0 ) + 0.95
+	$Light.color.a = alpha
+	
